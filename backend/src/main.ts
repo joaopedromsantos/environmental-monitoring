1a6a6a8e8b3f10d9bccd4a86d7a30be1a027aa92
joaopedromsantos <joaomartinscrf2019@gmail.com>
2025-08-07 16:58:29 -0300

    :sparkles feat: CORS added for localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-  const corsOrigin = configService.get<string>('CORS_ORIGIN') || 'http://localhost:3000';
+  const corsOrigin = configService.get<string>('CORS_ORIGIN') || 'http://localhost:5173';
 
   app.setGlobalPrefix('api');
 
